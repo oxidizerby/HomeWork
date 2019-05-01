2ba5e02de37cd8f908be15d563e8d366c6a10a3b
oxidizerby <oxidizer.by@gmail.com>
2019-05-01 07:10:32 +0300

    add task 6

--- homework2_6_2.py
+++ homework2_6_2.py
@@ -4,12 +4,9 @@ def task_6(s):
     print(s)
 
     l = len(s)//2
-
     result = True
     for i in range(l):
-
         print(s[i], s[~i])
-
         if s[i] != s[~i]:
             result = False
             break
