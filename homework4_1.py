a74266179af06df9ebce83002b7a48e538a5d464
oxidizerby <oxidizer.by@gmail.com>
2019-05-02 11:53:26 +0300

    add test3 and random

--- homework4_1.py
+++ homework4_1.py
@@ -4,7 +4,7 @@ n = 0
 for i in range(100):
     l = [random.choice([1, 2, 3, 4, 5, 7]) for i in range(2)]
     if l[0] == l[1]:
-        print(l, "________________________!!!")
+        print(l, "____________________________!!!")
         n+=1
     else: print(l)
 
