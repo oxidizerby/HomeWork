718e1e6e6df97f26461c9edd25ca40db09b98587
oxidizerby <oxidizer.by@gmail.com>
2019-05-07 10:37:53 +0300

    hw4_5

--- homework4_5/homework4_5.py
+++ homework4_5/homework4_5.py
@@ -1,4 +1,17 @@
+from decimal import Decimal
+from collections import defaultdict, namedtuple
 
-lst = [("Вася", "Иванов", (10, 8, 6)), ("Иван", "петров", (10, 10, 9, 9)), ("владимир", "иванов", (4, 5, 6))]
-print(lst)
+lst = [
+    ('Вася', 'Иванов', (10, 8, 6)),
+    ('Иван', 'петров', (10, 10, 9, 9)),
+    ('владимир', 'иванов', (4, 5, 6))
+]
+
+std = namedtuple('Student', ['name', 'surname', 'average_mark'])
+
+result = defaultdict(list)
+dd = defaultdict()
+for n, s, m, in lst:
+    result[(n[0]+s[0]).upper()] += [std(n, s, Decimal(sum(m)/len(m)))]
+print(result)
 
