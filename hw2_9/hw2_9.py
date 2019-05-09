b23fb06c76185ef2405366c271f149b0ca685f32
oxidizerby <oxidizer.by@gmail.com>
2019-05-09 14:52:11 +0300

    add hw2_9

--- hw2_9/hw2_9.py
+++ hw2_9/hw2_9.py
@@ -1,10 +1,22 @@
-lst = {1: 'BYN', 2: 'USD', 3: 'EUR'}
-x = input('Введите сумму: ')
-c = input(f'Введите номер валюты с которой конертируем  {lst} : ')
-r = input(f'Введите номер валюты в которую конертируем  {lst} : ')
+lst = ['BYN', 'USD', 'EUR']
+sum = float(input('Введите сумму: '))
+inp = input(f'Введите валюту с которой конвертируем {lst}: ').upper()
+out = input(f'Введите валюту в которую конвертируем {lst}: ').upper()
+
+ex = {'BYNUSD': 0.5, 'USDBYN': 2, 'BYNEUR': 0.4, 'EURBYN': 2.5, 'USDEUR': 0.8, 'EURUSD': 1.25}
+
+res = sum * ex[inp + out]
+print(f"{sum} {inp} = {res:.2f} {out}")
 
 # 1 USD = 2 BYN
 # 1 EUR = 2.5 BYN
 # 1 EUR = 1.25 USD
 
-ex = {12: '/2', 13: '/2.5', 21: '*2', 23: '/1.25', 31: '*2.5', 32: '*1.25'}
+# USD = BYN * 0.5
+# BYN = USD * 2
+
+# EUR = BYN * 0.4
+# BYN = EUR * 2.5
+
+# EUR = USD * 0.8
+# USD = EUR * 1.25
