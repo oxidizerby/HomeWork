7e213b0d345a049f1219768eb56a36855f2695c8
oxidizerby <oxidizer.by@gmail.com>
2019-05-29 04:07:27 +0300

    add hw6_2

--- hw6_2/hw6_2.py
+++ hw6_2/hw6_2.py
@@ -1,33 +1,40 @@
 import os
 
 
-def finf(p, n=-1):
-    n += 1
+def finf(p):
     ls = os.listdir(p)
     dd = {'FOLDERS': {}, 'FILES': {}}
-    d = {}
+    dfolder = {}
+    dfile = {}
     for name in ls:
         fname = os.path.join(p, name)
         if os.path.isdir(fname):
-            print('\t'*n + 'FOLDER: ' + name)
-            d[name] = finf(fname, n)
-            dd['FOLDERS'] = d
+            dfolder[name] = finf(fname)
+            dd['FOLDERS'] = dfolder
         if os.path.isfile(fname):
             ff = os.path.splitext(name)[1][1:]
-            d[name] = {'Format': ff, 'Size': str(os.stat(fname).st_size)}
-            print('\t'*n + 'file: ' + name)
-            print('\t' * (n+1) + 'Format: ' + ff)
-            print('\t' * (n+1) + 'Size: ' + str(os.stat(fname).st_size))
+            dfile[name] = {'Format': ff, 'Size': str(os.stat(fname).st_size)}
             if ff.lower() == 'txt':
                 fsb = open(fname)
                 fsb.seek(0, 2)
-                print('\t' * (n + 1) + 'Length: ' + str(fsb.tell()))
-                d[name]['Length'] = str(fsb.tell())
+                dfile[name]['Length'] = str(fsb.tell())
                 fsb.close()
-            dd['FILES'] = d
+            dd['FILES'] = dfile
     return dd
 
 
-print(finf('./venv'))
+def printdict(prndtc, tabs):
+    tabs += 1
+    for prn in prndtc:
+        if str(type(prndtc[prn])) == "<class 'dict'>":
+            print('\t' * tabs, prn)
+            printdict(prndtc[prn], tabs)
+        else:
+            print('\t' * tabs, prn, prndtc[prn])
+    pass
 
+
+pch = finf('./venv/lib/python3.7/site-packages/pip-19.0.3-py3.7.egg/pip/_internal')
+print(pch)
+printdict(pch, -1)
 
